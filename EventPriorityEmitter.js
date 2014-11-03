3798a697e052fa756b86704e4f474cf7d2287cde
Rafael De Leon <rafaelideleon@gmail.com>
2014-11-02 19:59:41 -0800

    New readme with examples and amd, nodejs, and browser support

--- EventPriorityEmitter.js
+++ EventPriorityEmitter.js
@@ -1,28 +1,179 @@
-var EventPriorityEmitter = (function (w) {
-    'use strict';
+/**
+ * Public interface for using EventPriorityEmitter.
+ *
+ * @method EventPriorityEmitter
+ * 
+ * @param {String}          [eventName=]          Name to give to the event.
+ *
+ * @param {Object|Function} [options]             An object for options or function to subscribe. Certain options
+ *                                                trigger certain actions, other non-related options will be discarded.
+ *
+ * @param {Object}          [options.pub]         For event publishing. If eventName is passed
+ *                                                and options, options.unSub, or options.pub are undefined,
+ *                                                then options.pub gets set to {} and the event publishes.
+ *
+ * @param {Boolean}         [options.rePub]       If set to true and subscribing to an event and the event had
+ *                                                published in the past, then re-publish for this subscriber
+ *                                                using the previous options.pub
+ *
+ * @param {String}          [options.unSub]       Required for un-subscribing from priority list.
+ *                                                The string refers to the subId to remove from list of priorities.
+ *
+ * @param {Function}        [options.sub]         Required for subscribing to an event.
+ *
+ * @param {String}          [options.subId]       Optional for subscribing. Use for identifying and removing
+ *                                                from priority list. Randomly generated if not defined when
+ *                                                subscribing (options.sub or options is a function).
+ *
+ * @param {int}             [options.priority]    0-11 where 0 is the lowest (last subscriber to get published data) priority 
+ *                                                and 11 is the highest (first subscriber to get published data). 
+ *                                                Every subscription will append to the list of priorities, 
+ *                                                except for options.timing=1 since there can be only one default.
+ *                                                If subscribing and options.priority is not set, 0 is be used. 
+ *                                                This is ignored when options.timing=1.
+ *
+ * @param {String}          [options.timing]      When the priority should happen.
+ *                                                pre = before default timing. There can be many of these timings.
+ *                                                def = default publish event. There is only one default timing.
+ *                                                post = after default event. There can be many of these timings.
+ *                                                If subscribing and options.timing is not set, 2 will be used.
+ *
+ *
+ * @returns {undefined|Function}                  "new EventPriorityEmitter" returns a function with a new
+ *                                                instance of EventPriorityEmitter for private use.
+ *                                                Otherwise, it will publish, subscribe or un-subscribe to
+ *                                                global EventPriorityEmitter and return undefined.
+ *
+ * @example
+ *
+ * //subscribe using default config
+ * EventPriorityEmitter(
+ *      'myGlobalEvent',
+ *      function (args) {
+ *          console && console.log && console.log(args);
+ *      }
+ * );
+ *
+ * //subscribe default timing
+ * EventPriorityEmitter(
+ *      'myGlobalEvent',
+ *      {
+ *          "timing" : "def",
+ *          "sub" : function (args) {
+ *              if (args && args.stuff) {
+ *                  //do something by default
+ *              }
+ *          }
+ *      }
+ * );
+ *
+ * //subscribe post timing
+ * EventPriorityEmitter(
+ *      'myGlobalEvent',
+ *      {
+ *          "subId": "sub id 2",
+ *          "timing" : "post",
+ *          "priority": 11,
+ *          "sub" : function (args) {
+ *              if (args && args.somethingelse) {
+ *                  //do something after default and before other priorities
+ *              }
+ *          }
+ *      }
+ * );
+ *
+ * //publish object with data inside the you want subscribers to consume
+ * EventPriorityEmitter(
+ *      'myGlobalEvent',
+ *      {
+ *          "pub": {
+ *              "stuff" : "data"
+ *          }
+ *      }
+ * );
+ *
+ * //un-subscribe a subId
+ * EventPriorityEmitter(
+ *      'myGlobalEvent',
+ *      {
+ *          "unSub": "sub id 2"
+ *      }
+ * );
+ *  
+ */
+(function (root, factory) {
+    var ns = 'EventPriorityEmitter';
+    //boilerplate node and browser defining.
+    if (typeof define === 'function' && define.amd) {
+        define(ns, factory(root));
+    } else if (typeof exports === 'object') {
+        module.exports = factory(root);
+    } else {
+        root[ns] = factory(root);
+    }
 
+} (this, function (root) {
+    'use strict';
+    /* global console */
     var PRIORITY_TYPE = ['pre', 'def', 'post'],
         PRIORITY_LIMIT = 11,
         fnArgsToArr = Array.prototype.slice,
-        fnIndexOf = Array.prototype.indexOf,
+        fnIndexOf = Array.prototype.indexOf || function (el) {
+            //supporting older browsers
+            var len,
+                i;
+
+            if (this && (len = this.length)){
+                i = 0;
+                //possible todo: faster and more efficient loop
+                for (; i < len; i++) {
+                    if (this[i] === el) {
+                        return i;
+                    }
+                }
+            }
+
+            return -1;
+        },
+        //host global usage for EPE
         _globalEventPriorities;
 
-
+    /**
+     * @private
+     * @desc private console output. Currently logs by default.
+     * @todo: think of good way to toggle logging.
+     */
      function _debugLog() {
         var args;
-        if (w.console && w.console.log) {
+        if (console && console.log) {
             args = fnArgsToArr.call(arguments);
             args.unshift('EPE: ');
-            w.console.log(args);
+            console.log(args);
         }
     }
 
+    /**
+     * Makes sure value is within range.
+     * @private
+     * @param validate
+     * @param max
+     * @param min
+     * @returns {boolean}
+     */
     function _isValidRange(validate, max, min) {
         min = min || 0;
 
         return !isNaN(validate) && validate >= min && validate <= max;
     }
 
+    /**
+     * Where all subscribers and priorities are kept.
+     * @private
+     * @class Subscriptions
+     * @param eventName
+     * @param epbName
+     * @constructor
+     */
     function Subscriptions(eventName, epbName){
         var i = 0,
             timings = {},
@@ -30,23 +181,99 @@ var EventPriorityEmitter = (function (w) {
 
         for(type = PRIORITY_TYPE[i]; i < PRIORITY_TYPE.length; type = PRIORITY_TYPE[++i]){
 
+            //there can be only one default and it's set to null until def is set to a function.
             if (type === 'def') {
                 timings[type] = null;
                 continue;
             }
-
             timings[type] = [];
         }
-
+        /*
+            keeps all the subscriber data, i.e. options.sub and other future config
+            Example data structure:
+            this.subIds = {
+               "sub id 1" : {
+                    "subId" : "sub id 1",
+                    "timing" : "pre",
+                    "priority": 0,
+                    "sub" : function () {[code]},
+                    ...
+               },
+               "sub id 2" : {
+                    "subId" : "sub id 2",
+                    "timing" : "post",
+                    "priority": 11,
+                    "sub" : function () {[code]},
+                    ...
+               },
+               "pr-120334234" : {
+                    "subId" : "pr-120334234",
+                    "timing" : "def",
+                    "priority": 2,
+                    "sub" : function () {[code]},
+                    ...
+               },
+               "sub id 3" : {
+                    "subId" : "sub id 3",
+                    "timing" : "pre",
+                    "priority": 5,
+                    "sub" : function () {[code]},
+                    ...
+               },
+               "sub id 4" : {
+                    "subId" : "sub id 4",
+                    "timing" : "pre",
+                    "priority": 5,
+                    "sub" : function () {[code]},
+                    ...
+               },
+               ...
+            }
+            Possible todo is to remove extra data that isn't used from the config.
+         */
         this.subIds = {};
+        //used for subscribers to know if data has been published and needs to be re-published.
         this.hasPub = false;
+        //if publication happens, oldArgs is kept for future subscribers that want to use past published data.
         this.oldArgs = {};
+        /* 
+            pre and post will be an array with length PRIORITY_LIMIT of arrays of unlimited size
+            containing subIds.
+            Example data structure:
+            this.timings = {
+                "pre" : [
+                    [ // index = 0
+                        "sub id 1"
+                    ],
+                    undefined,
+                    ...
+                    [ // index = 5
+                        "sub id 4",
+                        "sub id 5"
+                    ]
+                ],
+                "def" : "pr-120334234",
+                "post" : [
+                    undefined, // index = 0
+                    ...
+                    [ // index = 11
+                        "sub id 3"
+                    ]
+                ]
+            }
+         */
         this.timings = timings;
+        //for console.log
         this.eventName = epbName + eventName + '-> ';
     }
 
     Subscriptions.prototype = {
         'constructor': Subscriptions,
+        /**
+         * Updates or adds subscriber id with new definition.
+         * @private
+         * @param {Object} config
+         */
         'replaceSubId' : function (config) {
             var timing,
                 priority;
@@ -64,13 +291,22 @@ var EventPriorityEmitter = (function (w) {
                 timing = this.timings[config.timing];
                 priority = timing[config.priority];
 
+                //list of priorities is only set to an array until it needs it.
                 if (typeof priority === 'undefined') {
                     priority = timing[config.priority] = [];
                 }
-
+                //all new subId are added to the end of the priority list
                 priority.push(config.subId);
             }
         },
+        /**
+         * Find and remove subId from list of priorities. 
+         * If untrack is true or undefined, then delete from list of subIds  
+         * @private
+         * @param {String} subId
+         * @param {undefined|Boolean} untrack
+         * @returns {Boolean}
+         */
         'removeSubId': function (subId, untrack) {
             var subIdData = this.subIds[subId],
                 indexOf,
@@ -90,7 +326,7 @@ var EventPriorityEmitter = (function (w) {
                     timing.length &&
                     (priority = timing[subIdData.priority]) &&
                     priority.length &&
-                    (indexOf = fnIndexOf.call(priority, subId)) >= 0 /*Array.prototype.indexOf > IE 8*/
+                    (indexOf = fnIndexOf.call(priority, subId)) >= 0
                 ){
                     priority.splice(indexOf, 1);
                 }
@@ -148,12 +384,22 @@ var EventPriorityEmitter = (function (w) {
     };
 
     /**
+     * @private
+     * @class EventPriorityEmitter
      * @constructor
+     * @param {String} EPEName name to use to identify what EventPriorityEmitter is firing for console log.
      */
     function EventPriorityEmitter(EPEName) {
         this.epeName = '';
+        /*
+            keeps track of all event names that have subscriptions.
+            Example data structure
+            this.events = {
+                "myGlobalEvent" : Subscription,
+                "myOtherEvent": Subscription
+            };
+         */
         this.events = {};
-
 
         if (typeof EPEName === 'string') {
             this.epeName += EPEName;
@@ -181,7 +427,8 @@ var EventPriorityEmitter = (function (w) {
 
                 event = this.getEvent(eventName);
 
-                if (typeof config.subId !== 'string') {
+                //create random id if subId is defined for config tracking.
+                if (typeof config.subId !== 'string') {                    
                     config.subId = 'pr-' + Math.ceil(Math.random() * 10000000);
                 }
 
@@ -194,7 +441,7 @@ var EventPriorityEmitter = (function (w) {
 
                 temp = fnIndexOf.call(PRIORITY_TYPE, config.timing);
                 if(temp < 0) {
-                    config.timing = PRIORITY_TYPE[PRIORITY_TYPE.length - 1];
+                    config.timing = PRIORITY_TYPE[0]; //default config timing is pre
                 } else {
                     config.timing = PRIORITY_TYPE[temp];
                 }
@@ -213,10 +460,6 @@ var EventPriorityEmitter = (function (w) {
 
             return null;
         },
-        /**
-         * @param subId
-         * @param eventName
-         */
         'unSub': function (eventName, subId) {
             var event = this.getEvent(eventName);
             if (event) {
@@ -224,6 +467,12 @@ var EventPriorityEmitter = (function (w) {
                 event.removeSubId(subId);
             }
         },
+        /**
+         * Return or create new Subscription.
+         * @param  {String} eventName
+         * @return {Subscription}
+         * @private
+         */
         'getEvent': function (eventName) {
             var event = this.events[eventName];
 
@@ -234,6 +483,11 @@ var EventPriorityEmitter = (function (w) {
 
             return event;
         },
+        /**
+         * Determines what type of action to do based on what the options are.
+         * @param  {String} eventName
+         * @param  {Object|Function} options
+         */
         'exec': function(eventName, options) {
             options = options || {};
 
@@ -248,13 +502,13 @@ var EventPriorityEmitter = (function (w) {
 
                 if (typeof options.unSub === 'string') {
                     this.unSub(eventName, options.unSub);
-                } else if (typeof options.sub === 'function') { //subscribe to priorityName
+                } else if (typeof options.sub === 'function') { //subscribe to eventName
 
                     if(this.sub(eventName, options) === null) {
                         _debugLog(this.epeName + 'Subscription definition was invalid and was not registered');
                     }
 
-                } else { //publish to priorityName
+                } else { //publish to eventName
                     options = options.pub || options;
                     this.pub(eventName, options);
                 }
@@ -264,54 +518,13 @@ var EventPriorityEmitter = (function (w) {
 
     _globalEventPriorities = new EventPriorityEmitter('GLOBAL');
 
-    /**
-     * @param {String}          [eventName=]          Name to give to the event.
-     *
-     * @param {Object|Function} [options]             An object for options or function to subscribe. Certain options
-     *                                                trigger certain actions, other non-related options will be discarded.
-     *
-     * @param {Object}          [options.pub]         For event publishing. If eventName is passed
-     *                                                and options, options.unSub, or options.pub are undefined,
-     *                                                then options.pub gets set to {} and the event publishes.
-     *
-     * @param {Boolean}         [options.rePub]       If set to true and subscribing to an event and the event had
-     *                                                published in the past, then re-publish for this subscriber
-     *                                                using the previous options.pub
-     *
-     * @param {String}          [options.unSub]       Required for un-subscribing from priority list.
-     *                                                The string refers to the subId to remove from list of priorities.
-     *
-     * @param {Function}        [options.sub]         Required for subscribing to an event. Where coding happens.
-     *
-     * @param {String}          [options.subId]       Optional for subscribing. Use for identifying and removing
-     *                                                from priority list. Randomly generated if not defined when
-     *                                                subscribing (options.sub or options is a function).
-     *
-     * @param {int}             [options.priority]    0-11 where 0 is the lowest (last to publish) priority and
-     *                                                11 is the highest (first to publish). Every subscription will
-     *                                                append to the list of priorities, except for options.timing=1.
-     *                                                If subscribing and options.priority is not set, 0 be used.
-     *                                                This option is ignored by options.timing=1.
-     *
-     * @param {int}             [options.timing]      When the priority should happen.
-     *                                                pre = before default timing. There can be many of these timings.
-     *                                                def = default publish event. There is only one default timing.
-     *                                                post = after default event. There can be many of these timings.
-     *                                                If subscribing and options.timing is not set, 2 will be used.
-     *
-     *
-     * @returns {undefined|Function}                  "new EventPriorityEmitter" returns a function with a new
-     *                                                instance of EventPriorityEmitter for private use.
-     *                                                Otherwise, it will publish, subscribe or un-subscribe to
-     *                                                global EventPriorityEmitter and return undefined.
-     */
     return function (privateEPE) {
-        if (typeof this === 'undefined' || this === w) {
+        if (typeof this === 'undefined' || this === window) {
             _globalEventPriorities.exec.apply(
                 _globalEventPriorities,
                 fnArgsToArr.call(arguments)
             );
-        } else {
+        } else if (typeof privateEPE === 'string') {
             //return new wrapper
             return (function () {
                 var EPE = new EventPriorityEmitter(privateEPE);
@@ -325,4 +538,5 @@ var EventPriorityEmitter = (function (w) {
             }());
         }
     };
-}(window));
+}));
+
--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-EventPriorityBroadCaster
-========================
-Work in progress
+<a name="EventPriorityEmitter"></a>
+#EventPriorityEmitter([eventName=], [options])
+Public interface for using EventPriorityEmitter.
+
+**Params**
+
+- \[eventName=\] `String` - Name to give to the event.  
+- \[options\] `Object` | `function` - An object for options or function to subscribe. Certain options                                               trigger certain actions, other non-related options will be discarded.  
+  - \[pub\] `Object` - For event publishing. If eventName is passed                                               and options, options.unSub, or options.pub are undefined,                                               then options.pub gets set to {} and the event publishes.  
+  - \[rePub\] `Boolean` - If set to true and subscribing to an event and the event had                                               published in the past, then re-publish for this subscriber                                               using the previous options.pub  
+  - \[unSub\] `String` - Required for un-subscribing from priority list.                                               The string refers to the subId to remove from list of priorities.  
+  - \[sub\] `function` - Required for subscribing to an event.  
+  - \[subId\] `String` - Optional for subscribing. Use for identifying and removing                                               from priority list. Randomly generated if not defined when                                               subscribing (options.sub or options is a function).  
+  - \[priority\] `int` - 0-11 where 0 is the lowest (last subscriber to get published data) priority                                                and 11 is the highest (first subscriber to get published data).                                                Every subscription will append to the list of priorities,                                                except for options.timing=1 since there can be only one default.                                               If subscribing and options.priority is not set, 0 is be used.                                                This is ignored when options.timing=1.  
+  - \[timing\] `String` - When the priority should happen.                                               pre = before default timing. There can be many of these timings.                                               def = default publish event. There is only one default timing.                                               post = after default event. There can be many of these timings.                                               If subscribing and options.timing is not set, 2 will be used.  
+
+**Returns**: `undefined` | `function` - "new EventPriorityEmitter" returns a function with a new                                               instance of EventPriorityEmitter for private use.                                               Otherwise, it will publish, subscribe or un-subscribe to                                               global EventPriorityEmitter and return undefined.  
+**Example**  
+//subscribe using default configEventPriorityEmitter(     'myGlobalEvent',     function (args) {         console && console.log && console.log(args);     });//subscribe default timingEventPriorityEmitter(     'myGlobalEvent',     {         "timing" : "def",         "sub" : function (args) {             if (args && args.stuff) {                 //do something by default             }         }     });//subscribe post timingEventPriorityEmitter(     'myGlobalEvent',     {         "subId": "sub id 2",         "timing" : "post",         "priority": 11,         "sub" : function (args) {             if (args && args.somethingelse) {                 //do something after default and before other priorities             }         }     });//publish object with data inside the you want subscribers to consumeEventPriorityEmitter(     'myGlobalEvent',     {         "pub": {             "stuff" : "data"         }     });//un-subscribe a subIdEventPriorityEmitter(     'myGlobalEvent',     {         "unSub": "sub id 2"     }); 
+
